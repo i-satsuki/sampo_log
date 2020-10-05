064e18e56b9d08851b4aad4565bcb73a2505e9db
vagrant <vagrant@localhost.localdomain>
2020-10-05 11:28:31 +0000

    [Add]user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,48 @@
 class UsersController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update, :unsubscribe, :withdraw]
+
   def show
+  	@user = User.find(params[:id])
+  	# 退会ユーザーの情報は表示しない
+  	if @user.is_deleted == true
+		redirect_to user_path(current_user)
+	end
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "更新に成功しました！"
+    else
+      render "edit"
+    end
+  end
+
+  def unsubscribe
+  	@user = User.find(params[:id])
+  end
+
+  def withdraw
+  	@user = User.find(params[:id])
+    if @user.update(is_deleted: true)
+    	sign_out current_user
+	end
+    redirect_to root_path, notice: "退会処理が完了しました！"
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+  attachment :profile_image
 
   validates :name, presence: true
   validates :uid, presence: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
+
+  # is_deletedがfalseの場合（有効会員）、ログイン可能
+  def active_for_authentication?
+      super && (self.is_deleted == false)
+  end
 end
