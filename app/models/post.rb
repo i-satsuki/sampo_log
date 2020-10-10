ba272a8be60e8cd7cc77887ccbb63b75c8d2859a
vagrant <vagrant@localhost.localdomain>
2020-10-10 12:05:08 +0000

    [Add]favorite

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,14 @@
 class Post < ApplicationRecord
 	belongs_to :user
+	has_many :favorites
+
 	attachment :image
 
 	validates :title, presence: true
 	validates :body, presence: true
 	validates :steps, presence: true, numericality: true
+
+	def favorited_by?(user)
+      favorites.where(user_id: user.id).exists?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :posts
+  resources :posts do
+    resource :favorites, only: [:create, :destroy]
+  end
 
   resources :users, only: [:show, :edit, :update]do
       resource :relationships, only: [:create, :destroy]
